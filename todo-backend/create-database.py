5867f8f7f013e18e1a0d5df2aea9e6a0da118d28
Marcos <marcosmartostuvi@gmail.com>
2025-05-19 21:10:58 -0300

    configuracion env back y carpeta dist front

--- todo-backend/create-database.py
+++ todo-backend/create-database.py
@@ -21,7 +21,8 @@ conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
 )
 
 # Creamos un cursor para ejecutar comandos SQL
